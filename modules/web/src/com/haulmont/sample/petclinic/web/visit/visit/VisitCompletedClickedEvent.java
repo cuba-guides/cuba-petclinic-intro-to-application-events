989311fe4b7e5dc2b8f06da33179975e44ccaa31
Mario <mario@die-davids.de>
2020-08-25 09:43:00 +0200

    align guide with project

--- modules/web/src/com/haulmont/sample/petclinic/web/visit/visit/VisitCompletedClickedEvent.java
+++ modules/web/src/com/haulmont/sample/petclinic/web/visit/visit/VisitCompletedClickedEvent.java
@@ -1,7 +1,6 @@
 package com.haulmont.sample.petclinic.web.visit.visit;
 
 import com.haulmont.cuba.gui.events.UiEvent;
-import com.haulmont.sample.petclinic.entity.visit.Visit;
 import org.springframework.context.ApplicationEvent;
 
 
